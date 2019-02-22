febafa60781d28a4420bdc93bc70d408692d1196
Steven (Steven) Deutekom <deutekom@uleth.ca>
2019-02-22 10:29:03 -0700

    Added empty implementation for game and added Failing tests for TestGame

---
+++ src/Game.cpp
@@ -0,0 +1,39 @@
+/*
+ * CPSC2720 Group Project Spring 2019
+ * Steven Deutekom <deutekom@uleth.ca>
+ * Max Niu <max.niu@uleth.ca>
+ */
+
+#include "Game.h"
+#include "Player.h"
+#include "Room.h"
+
+
+Game::Game() {
+
+}
+
+virtual Game::~Game() {
+
+}
+
+void Game::run() {
+
+}
+
+Player& Game::getPlayer() const {
+  return *player;
+}
+
+void Game::setPlayer(Player* p) {
+
+}
+
+Room* Game::getRoom(string name) const {
+  Room* r;
+  return r;
+}
+
+void Game::addRoom(Room* room) {
+
+}
---
+++ test/TestGame.cpp
@@ -0,0 +1,28 @@
+/*
+ * CPSC2720 Group Project Spring 2019
+ * Steven Deutekom <deutekom@uleth.ca>
+ * Max Niu <max.niu@uleth.ca>
+ */
+
+ #include "Game.h"
+
+
+ TEST(GameTests, constructor) {
+  FAIL();
+ }
+
+  TEST(GameTests, get_player) {
+  FAIL();
+ }
+
+  TEST(GameTests, set_player) {
+  FAIL();
+ }
+
+  TEST(GameTests, get_room) {
+  FAIL();
+ }
+
+  TEST(GameTests, add_room) {
+  FAIL();
+ }
