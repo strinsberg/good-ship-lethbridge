715376c0a3187d1529f6ca50cff19e199b5ddbfb
Zeyuan (Max) Niu <nium2720@36punisher.cs.uleth.ca>
2019-03-09 15:22:26 -0700

    EntityState tested

---
+++ test/EntityStateTest.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "EntityState.h"
+#include "Exceptions.h"
+
+TEST(TestEntityState,EntityState) {
+EntityState e;
+EXPECT_EQ(true,e.getActive());
+EXPECT_EQ(true,e.getObtainable());
+EXPECT_EQ(false,e.getHidden());
+}
+
+TEST(TestEntityState,setObtainable) {
+EntityState e;
+e.setObtainable(false);
+
+EXPECT_EQ(false,e.getObtainable());
+}
+
+TEST(TestEntityState,setActive) {
+EntityState e;
+e.setActive(false);
+
+EXPECT_EQ(false,e.getActive());}
+
+TEST(TestEntityState,setHidden) {
+EntityState e;
+e.setHidden(true);
+
+EXPECT_EQ(true,e.getHidden());}
