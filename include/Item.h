1bba9920e47152124a3941f269ac6d58dee69916
Steven (Steven) Deutekom <deutekom@uleth.ca>
2019-03-12 09:08:09 -0600

    Added Item class and implemented. Passing all tests.

---
+++ include/Item.h
@@ -0,0 +1,32 @@
+/**
+ * CPSC2720 Group Project Spring 2019
+ * @author Steven Deutekom <deutekom@uleth.ca>, Max Niu <max.niu@uleth.ca>
+ * @date 2019-03-12
+ */
+
+#ifndef ITEM_H
+#define ITEM_H
+
+#include "Entity.h"
+#include "ObjectBlueprint.h"
+#include <string>
+
+
+class Item : public Entity
+{
+  public:
+    Item();
+    virtual ~Item();
+
+    virtual std::string describe() const;
+
+    virtual std::string use(Entity* user);
+
+    virtual ObjectBlueprint* makeBlueprint() const;
+
+  private:
+    Item(const Item& other);
+    Item& operator=(const Item& other);
+};
+
+#endif // ITEM_H
