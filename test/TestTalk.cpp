37105b35d764af03bb87a4b1db18dee5407e8c82
Steven (Steven) Deutekom <deutekom@uleth.ca>
2019-03-13 16:35:22 -0600

    Finished talk class. All tests Pass

--- test/TestTalk.cpp
+++ test/TestTalk.cpp
@@ -5,6 +5,9 @@
  */
 
 #include "Talk.h"
+#include "Npc.h"
+#include "Inform.h"
+#include "Item.h"
 #include <string>
 #include "gtest/gtest.h"
 
@@ -25,14 +28,69 @@ TEST(TalkTests, set_noun) {
 }
 
 
-TEST(TalkTests, DISABLED_execute_item_exists) {
+TEST(TalkTests, execute_item_exists) {
+  Player* p = new Player();
+  Npc* npc = new Npc();
+  npc->getSpec()->setName("ted");
+  Room* room = new Room();
+  room->addEntity(npc);
+  p->setCurrentRoom(room);
+  Inform* tell = new Inform();
+  tell->setMessage("Hi, how are you?");
+  npc->setEvent(tell);
+  Talk talk(p);
+  talk.setNoun("ted");
 
+  EXPECT_EQ("Hi, how are you?", talk.execute());
+  delete p;
+  delete room;
 }
 
-TEST(TalkTests, DISABLED_execute_does_not_talk) {
+TEST(TalkTests, execute_inactive) {
+  Player* p = new Player();
+  Npc* npc = new Npc();
+  npc->getSpec()->setName("ted");
+  npc->getState()->setActive(false);
+  Room* room = new Room();
+  room->addEntity(npc);
+  p->setCurrentRoom(room);
+  Inform* tell = new Inform();
+  tell->setMessage("Hi, how are you?");
+  npc->setEvent(tell);
+  Talk talk(p);
+  talk.setNoun("ted");
 
+  EXPECT_EQ("ted can't talk right now", talk.execute());
+  delete p;
+  delete room;
 }
 
-TEST(TalkTests, DISABLED_execute_item_does_not_exist) {
+TEST(TalkTests, execute_item_does_not_exist) {
+  Player* p = new Player();
+  Room* room = new Room();
+  p->setCurrentRoom(room);
+  Talk talk(p);
+  talk.setNoun("ted");
 
+  EXPECT_EQ("There is no ted", talk.execute());
+  delete p;
+  delete room;
+}
+
+TEST(TalkTests, execute_not_an_npc) {
+  Player* p = new Player();
+  Item* i = new Item();
+  i->getSpec()->setName("box");
+  i->getState()->setActive(false);
+  Room* room = new Room();
+  room->addEntity(i);
+  p->setCurrentRoom(room);
+  Inform* tell = new Inform();
+  tell->setMessage("Hi, how are you?");
+  i->setEvent(tell);
+  Talk talk(p);
+  talk.setNoun("box");
+
+  EXPECT_EQ("You can't talk to box", talk.execute());
+  delete p;
 }
